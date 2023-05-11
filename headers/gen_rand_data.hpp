b92269cffd1a3e1cda313b1db7ab10a26745f8c2
ArtyomkaKuznetsov <artyomka_working@mail.ru>
2023-05-11 14:18:21 +0300

    removed code duplication

--- headers/gen_rand_data.hpp
+++ headers/gen_rand_data.hpp
@@ -10,19 +10,40 @@
 namespace gen_rand_data
 {
 	template<typename T>
-	bool checkRange(const T& min, const T& max)
-	{
+	bool checkRange(const T& min, const T& max) {
 		return min <= max;
 	}
 
+	template<typename T, typename... Args>
+	struct is_any_of {
+		static constexpr bool value = std::disjunction<std::is_same<T, Args>...>::value;
+	};
 
+	template<typename T>
+	struct is_integral_t {
+		static constexpr bool value = std::is_integral<T>::value &&
+			!is_any_of<T, int8_t, uint8_t, char, bool>::value;
+	};
+
+	template<typename T>
+	struct is_float_point_t {
+		static constexpr bool value = std::is_floating_point<T>::value;
+	};
+
+	template<typename T>
+	struct is_char_t {
+		static constexpr bool value = is_any_of<T, char, int8_t, uint8_t>::value;
+	};
+	
+
+	template<typename T, typename Enable = void>
+	class Generator {};
 
 
 	/////////////////////// INTEGER TYPES ///////////////////////
 
 	template<typename T>
-	class Generator
-	{
+	class Generator<T, typename std::enable_if<is_integral_t<T>::value>::type> {
 	private:
 		std::mt19937_64 gen{ std::random_device{}() };
 		std::uniform_int_distribution<T> dist;
@@ -38,128 +59,47 @@ namespace gen_rand_data
 		T operator()() { return dist(gen); }
 	};
 
-	template<typename T>
-	inline void Generator<T>::setRange(const T& min, const T& max)
-	{
-		if (!checkRange(min, max))
-		{
-			throw std::logic_error{ "min_range > max_range" };
-		}
-
-		dist = std::uniform_int_distribution<T>(min, max);
-	}
-
 
 
 
 	/////////////////////// DOUBLE TYPES ///////////////////////
 
-	template<>
-	class Generator<float>
-	{
+	template<typename T>
+	class Generator<T, typename std::enable_if<is_float_point_t<T>::value>::type> {
 	private:
 		std::mt19937_64 gen{ std::random_device{}() };
-		std::uniform_real_distribution<float> dist;
+		std::uniform_real_distribution<T> dist;
 
 	public:
-		Generator(const float& min, const float& max) : dist{ min, max } {}
+		Generator(const T& min, const T& max) : dist{ min, max } {}
 
 		float getMin() noexcept(noexcept(dist.min())) { return dist.min(); }
 		float getMax() noexcept(noexcept(dist.max())) { return dist.max(); }
 
-		void setRange(const float& min, const float& max);
+		void setRange(const T& min, const T& max) {
+			if (!checkRange(min, max)) {
+				throw std::logic_error{ "min_range > max_range" };
+			}
+
+			dist = std::uniform_real_distribution<T>(min, max);
+		}
 
 		float operator()() { return dist(gen); }
 	};
-
-	inline void Generator<float>::setRange(const float &min, const float &max)
-    {
-		if (!checkRange(min, max))
-		{
-			throw std::logic_error{ "min_range > max_range" };
-		}
-
-		dist = std::uniform_real_distribution<float>(min, max);
-	}
-
-
-
-	template<>
-	class Generator<double>
-	{
-	private:
-		std::mt19937_64 gen{ std::random_device{}() };
-		std::uniform_real_distribution<double> dist;
-
-	public:
-		Generator(const double& min, const double& max) : dist{ min, max } {}
-
-		double getMin() noexcept(noexcept(dist.min())) { return dist.min(); }
-		double getMax() noexcept(noexcept(dist.max())) { return dist.max(); }
-
-		void setRange(const double& min, const double& max);
-
-		double operator()() { return dist(gen); }
-	};
-
-	inline void Generator<double>::setRange(const double& min, const double& max)
-    {
-		if (!checkRange(min, max))
-		{
-			throw std::logic_error{ "min_range > max_range" };
-		}
-
-		dist = std::uniform_real_distribution<double>(min, max);
-	}
-
-
-
-	template<>
-	class Generator<long double>
-	{
-	private:
-		std::mt19937_64 gen{ std::random_device{}() };
-		std::uniform_real_distribution<long double> dist;
-
-	public:
-		Generator(const long double& min, const long double& max) : dist{ min, max } {}
-
-		long double getMin() noexcept(noexcept(dist.min())) { return dist.min(); }
-		long double getMax() noexcept(noexcept(dist.max())) { return dist.max(); }
-
-		void setRange(const long double& min, const long double& max);
-
-		long double operator()() { return dist(gen); }
-	};
-
-
-	inline void Generator<long double>::setRange(const long double& min, const long double& max)
-    {
-		if (!checkRange(min, max))
-		{
-			throw std::logic_error{ "min_range > max_range" };
-		}
-
-		dist = std::uniform_real_distribution<long double>(min, max);
-	}
-
 
 
 
 	/////////////////////// CHAR TYPES ///////////////////////
 
-	template<>
-	class Generator <int8_t>
-	{
+	template<typename T>
+	class Generator <T, typename std::enable_if<is_char_t<T>::value>::type> {
 	private:
-		int8_t m_min_range;
-		int8_t m_max_range;
+		T m_min_range;
+		T m_max_range;
 
 	public:
-		Generator(const int8_t& min, const int8_t& max) : m_min_range{ min }, m_max_range{ max }
-		{
-			if (!checkRange(min, max))
-			{
+		Generator(const T& min, const T& max) : m_min_range{ min }, m_max_range{ max } {
+			if (!checkRange(min, max)) {
 				throw std::logic_error{ "min_range > max_range" };
 			}
 		}
@@ -167,59 +107,17 @@ namespace gen_rand_data
 		int8_t getMin() const noexcept { return m_min_range; }
 		int8_t getMax() const noexcept { return m_max_range; }
 
-		void setRange(const int8_t& min, const int8_t& max);
+		void setRange(const T& min, const T& max) {
+			if (!checkRange(min, max)) {
+				throw std::logic_error{ "minRange > maxRange" };
+			}
+
+			m_min_range = min;
+			m_max_range = max;
+		}
 
 		int8_t operator()() { return m_min_range + rand() % (m_max_range - m_min_range + 1); }
 	};
-
-    inline void Generator<int8_t>::setRange(const int8_t& min, const int8_t& max)
-    {
-		if (!checkRange(min, max))
-		{
-			throw std::logic_error{ "minRange > maxRange" };
-		}
-
-		m_min_range = min;
-		m_max_range = max;
-	}
-
-
-
-	template<>
-	class Generator <uint8_t>
-	{
-	private:
-		uint8_t m_min_range;
-		uint8_t m_max_range;
-
-	public:
-		Generator(const uint8_t& min, const uint8_t& max) : m_min_range{ min }, m_max_range{ max }
-		{
-			if (!checkRange(min, max))
-			{
-				throw std::logic_error{ "minRange > maxRange" };
-			}
-		}
-
-		uint8_t getMin() const noexcept { return m_min_range; }
-		uint8_t getMax() const noexcept { return m_max_range; }
-
-		void setRange(const uint8_t& min, const uint8_t& max);
-
-		uint8_t operator()() { return m_min_range + rand() % (m_max_range - m_min_range + 1); }
-	};
-
-    inline void Generator<uint8_t>::setRange(const uint8_t& min, const uint8_t& max)
-    {
-		if (!checkRange(min, max))
-		{
-			throw std::logic_error{ "minRange > maxRange" };
-		}
-
-		m_min_range = min;
-		m_max_range = max;
-	}
-
 
 } // namespace gen_rand_data
 
