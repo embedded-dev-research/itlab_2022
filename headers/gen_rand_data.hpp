331a4c4a00253313389cd323d3472b4f34124a7d
ArtyomkaKuznetsov <artyomka_working@mail.ru>
2023-05-11 15:12:28 +0300

    check clang-format test

--- headers/gen_rand_data.hpp
+++ headers/gen_rand_data.hpp
@@ -14,54 +14,56 @@
 
 _GEN_RAND_DATA_BEGIN
 
-template<typename T>
+template <typename T>
 bool checkRange(const T& min, const T& max) {
-	return min <= max;
+ return min <= max;
 }
 
-template<typename T, typename... Args>
+template <typename T, typename... Args>
 struct is_any_of {
-	static constexpr bool value = std::disjunction<std::is_same<T, Args>...>::value;
+	static constexpr bool value =
+	std::disjunction<std::is_same<T, Args>...>::value;
 };
 
-template<typename T>
+template <typename T>
 struct is_integral_t {
-	static constexpr bool value = std::is_integral<T>::value &&
-		!is_any_of<T, int8_t, uint8_t, char, bool>::value;
+ static constexpr bool value =
+	std::is_integral<T>::value &&
+	!is_any_of<T, int8_t, uint8_t, char, bool>::value;
 };
 
-template<typename T>
+template <typename T>
 struct is_float_point_t {
-	static constexpr bool value = std::is_floating_point<T>::value;
+ static constexpr bool value = std::is_floating_point<T>::value;
 };
 
-template<typename T>
+template <typename T>
 struct is_char_t {
-	static constexpr bool value = is_any_of<T, char, int8_t, uint8_t>::value;
+ static constexpr bool value = is_any_of<T, char, int8_t, uint8_t>::value;
 };
 	
 
-template<typename T, typename Enable = void>
+template <typename T, typename Enable = void>
 class Generator {};
 
 
 /////////////////////// INTEGER TYPES ///////////////////////
 
-template<typename T>
+template <typename T>
 class Generator<T, typename std::enable_if<is_integral_t<T>::value>::type> {
-private:
-	std::mt19937_64 gen{ std::random_device{}() };
-	std::uniform_int_distribution<T> dist;
+ private:
+  std::mt19937_64 gen{ std::random_device{}() };
+  std::uniform_int_distribution<T> dist;
 
-public:
-	Generator(const T& min, const T& max) : dist{ min, max } {}
+ public:
+  Generator(const T& min, const T& max) : dist{ min, max } {}
 
-	T getMin() noexcept(noexcept(dist.min())) { return dist.min(); }
-	T getMax() noexcept(noexcept(dist.max())) { return dist.max(); }
+  T getMin() noexcept(noexcept(dist.min())) { return dist.min(); }
+  T getMax() noexcept(noexcept(dist.max())) { return dist.max(); }
 
-	void setRange(const T& min, const T& max);
+  void setRange(const T& min, const T& max);
 
-	T operator()() { return dist(gen); }
+  T operator()() { return dist(gen); }
 };
 
 
@@ -69,7 +71,7 @@ public:
 
 /////////////////////// DOUBLE TYPES ///////////////////////
 
-template<typename T>
+template <typename T>
 class Generator<T, typename std::enable_if<is_float_point_t<T>::value>::type> {
 private:
 	std::mt19937_64 gen{ std::random_device{}() };
@@ -96,7 +98,7 @@ public:
 
 /////////////////////// CHAR TYPES ///////////////////////
 
-template<typename T>
+template <typename T>
 class Generator <T, typename std::enable_if<is_char_t<T>::value>::type> {
 private:
 	T m_min_range;
