da4d1ee02db94589ce3772253f4db8f6bd12fc83
IzmaylovI <75639514+IzmaylovI@users.noreply.github.com>
2023-05-14 01:35:56 +0300

    resolved_first_conflict

--- main.cpp
+++ main.cpp
@@ -4,16 +4,6 @@
 
 int main() {
   Image<bgr> a(1, 1);
-  float32_t* A = new float32_t[360000];
-  float32_t* B = new float32_t[360000];
-  float32_t* C = new float32_t[360000];
-
-  void (*q)(float32_t*, int, int) = print_matrx;
-
-  Benchmark<void (*)(float32_t*, float32_t*, float32_t*, int, int, int),
-            float32_t*, float32_t*, float32_t*, int, int, int>
-      b(matrix_multiply_2x2_neon_float, A, B, C, 600, 600, 600);
-  b.run();
-  b.info();
+  
   return 0;
 }
